624268c6ee2f51c67ed108888195c638cd6741cd
Hyun <hyun_7586@naver.com>
2025-08-14 21:05:06 +0900

    feat: 공통적으로 사용할 CustomException 객체 생성

---
+++ src/main/java/com/example/BE/global/exception/CustomException.java
@@ -0,0 +1,24 @@
+package com.example.BE.global.exception;
+
+import com.example.BE.global.exception.errorCode.ErrorCode;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class CustomException extends RuntimeException {
+    private final HttpStatus httpStatus;
+    private final String errorCode;
+    private final String errorMessage;
+
+    private CustomException(ErrorCode errorCode) {
+        super(errorCode.message());
+        this.httpStatus = errorCode.httpStatus();
+        this.errorCode = errorCode.code();
+        this.errorMessage = errorCode.message();
+    }
+
+    public static CustomException from(ErrorCode errorCode) {
+        return new CustomException(errorCode);
+    }
+
+}
