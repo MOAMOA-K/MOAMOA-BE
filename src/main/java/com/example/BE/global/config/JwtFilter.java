dd3e365215c5b24e5d13dde9ffe08ac55852338a
Hyun <hyun_7586@naver.com>
2025-08-15 17:58:04 +0900

    refactor: SecurityConfig에서 JwtFilter를 Bean으로 주입받도록 수정

--- src/main/java/com/example/BE/global/config/JwtFilter.java
+++ src/main/java/com/example/BE/global/config/JwtFilter.java
@@ -18,10 +18,12 @@ import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 @RequiredArgsConstructor
 @Slf4j
+@Component
 public class JwtFilter extends OncePerRequestFilter {
 
     private final JwtUtil jwtUtil;
