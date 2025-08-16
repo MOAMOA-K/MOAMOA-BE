4e980c523d134a4655709abd15587384dad14382
Hyun <hyun_7586@naver.com>
2025-08-16 21:04:16 +0900

    chore: 오타 수정

--- src/main/java/com/example/BE/global/exception/errorCode/MenuErrorCode.java
+++ src/main/java/com/example/BE/global/exception/errorCode/MenuErrorCode.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 public enum MenuErrorCode implements ErrorCode {
 
     MENU_NOT_FOUND(HttpStatus.NOT_FOUND, "M001", "존재하지 않는 메뉴입니다."),
-    FORBIDDEN_STORE_ACCESS(HttpStatus.FORBIDDEN, "M002", "해당 메뉴에 대한 접근 권한이 없습니다.");
+    FORBIDDEN_MENU_ACCESS(HttpStatus.FORBIDDEN, "M002", "해당 메뉴에 대한 접근 권한이 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
