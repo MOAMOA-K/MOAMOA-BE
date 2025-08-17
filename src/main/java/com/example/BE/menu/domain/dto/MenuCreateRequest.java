f2f6834962f278f2935accad3613c16533e63a38
Hyun <hyun_7586@naver.com>
2025-08-17 09:06:50 +0900

    refactor: Menu, Store 생성 dto에 @Size 제약조건 추가

--- src/main/java/com/example/BE/menu/domain/dto/MenuCreateRequest.java
+++ src/main/java/com/example/BE/menu/domain/dto/MenuCreateRequest.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
 
 public record MenuCreateRequest(
     @NotNull(message = "가게ID는 필수값입니다.")
@@ -11,10 +12,13 @@ public record MenuCreateRequest(
     Long storeId,
 
     @NotBlank(message = "메뉴명은 필수값입니다.")
+    @Size(max=32, message="메뉴명은 최대 32자입니다.")
     String name,
 
     @PositiveOrZero(message = "가격은 0원 이상이어야 합니다.")
     int price,
+
+    @Size(max=255, message="메뉴 설명은 최대 255자입니다.")
     String description,
     String imageUrl
 ) {}
