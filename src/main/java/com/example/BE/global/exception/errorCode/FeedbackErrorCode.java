368edcb64d3388b699a87867abaa22a4ea8de2c1
Hyun <hyun_7586@naver.com>
2025-08-17 10:26:48 +0900

    feat: AnnouncementService에서 참조를 위해 FeedbackErrorCode, FeedbackRepository 생성

---
+++ src/main/java/com/example/BE/global/exception/errorCode/FeedbackErrorCode.java
@@ -0,0 +1,30 @@
+package com.example.BE.global.exception.errorCode;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@RequiredArgsConstructor
+public enum FeedbackErrorCode implements ErrorCode {
+
+    FEEDBACK_NOT_FOUND(HttpStatus.NOT_FOUND, "F001", "존재하지 않는 피드백입니다."),
+    FORBIDDEN_FEEDBACK_ACCESS(HttpStatus.FORBIDDEN, "F002", "해당 피드백에 대한 접근 권한이 없습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+
+    @Override
+    public HttpStatus httpStatus() {
+        return this.httpStatus;
+    }
+
+    @Override
+    public String code() {
+        return this.code;
+    }
+
+    @Override
+    public String message() {
+        return this.message;
+    }
+}
