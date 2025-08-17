47344fb54367063b598598f0e7d92ef3241fed3a
Hyun <hyun_7586@naver.com>
2025-08-17 12:09:40 +0900

    refactor: 가게 생성/수정/삭제할 때 본인 가게에만 접근할 수 있도록 검증 로직 추가

--- src/main/java/com/example/BE/store/service/StoreService.java
+++ src/main/java/com/example/BE/store/service/StoreService.java
@@ -1,21 +1,22 @@
 package com.example.BE.store.service;
 
+import com.example.BE.announcement.repository.AnnouncementRepository;
 import com.example.BE.global.exception.CustomException;
+import com.example.BE.global.exception.errorCode.AnnouncementErrorCode;
 import com.example.BE.global.exception.errorCode.StoreErrorCode;
+import com.example.BE.menu.repository.MenuRepository;
 import com.example.BE.store.domain.StoreEntity;
 import com.example.BE.store.domain.dto.StoreCreateRequest;
 import com.example.BE.store.domain.dto.StoreDetailResponse;
 import com.example.BE.store.domain.dto.StoreResponse;
 import com.example.BE.store.domain.dto.StoreUpdateRequest;
 import com.example.BE.store.repository.StoreRepository;
-import java.util.Objects;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -24,12 +25,14 @@ import java.util.stream.Collectors;
 public class StoreService {
 
     private final StoreRepository storeRepository;
-    //private final MenuRepository menuRepository; // 상세 조회를 위해 주입
-    //private final StoreAnnouncementRepository announcementRepository; // 상세 조회를 위해 주입
+    private final MenuRepository menuRepository;
+    private final AnnouncementRepository announcementRepository;
 
     @Transactional
     public StoreResponse createStore(StoreCreateRequest request, Long userId) {
         StoreEntity newStore = StoreEntity.of(userId, request);
+        validateStoreOwner(newStore, userId);
+
         return StoreResponse.from(storeRepository.save(newStore));
     }
 
@@ -43,21 +46,16 @@ public class StoreService {
 
     public List<StoreResponse> searchStores(double lat, double lon, String keyword) {
         final int radius = 2000; // 2km 반경으로 고정
-        List<StoreEntity> stores = storeRepository.findNearbyStoresWithKeyword(lat, lon, radius, keyword);
+        List<StoreEntity> stores = storeRepository.findNearbyStoresWithKeyword(lat, lon, radius,
+            keyword);
         return stores.stream().map(StoreResponse::from).collect(Collectors.toList());
     }
 
     @Transactional
     public StoreResponse updateStore(Long storeId, StoreUpdateRequest request, Long userId) {
         StoreEntity store = findByIdOrThrow(storeId);
-        // 가게 주인 본인인지 확인
-//        log.info("storeId: {}", storeId);
-//        log.info("store userId: {}", store.getUserId());
-//        log.info("request userId: {}", userId);
+        validateStoreOwner(store, userId);
 
-        if (!store.getUserId().equals(userId)) {
-            throw CustomException.from(StoreErrorCode.FORBIDDEN_STORE_ACCESS);
-        }
         store.update(request);
         return StoreResponse.from(store);
     }
@@ -65,10 +63,7 @@ public class StoreService {
     @Transactional
     public void deleteStore(Long userId, Long storeId) {
         StoreEntity store = findByIdOrThrow(storeId);
-
-        if(!store.getUserId().equals(userId)){
-            throw CustomException.from(StoreErrorCode.FORBIDDEN_STORE_ACCESS);
-        }
+        validateStoreOwner(store, userId);
 
         storeRepository.deleteById(storeId);
     }
@@ -76,5 +71,11 @@ public class StoreService {
     private StoreEntity findByIdOrThrow(Long storeId) {
         return storeRepository.findById(storeId)
             .orElseThrow(() -> CustomException.from(StoreErrorCode.STORE_NOT_FOUND));
+    }
+
+    private void validateStoreOwner(StoreEntity store, Long userId) {
+        if (!store.getUserId().equals(userId)) {
+            throw CustomException.from(StoreErrorCode.FORBIDDEN_STORE_ACCESS);
+        }
     }
 }
