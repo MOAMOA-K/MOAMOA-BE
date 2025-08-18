ef0ccb591a9cbabe182c965ffb635ceb0aa9bd8b
jbh010204 <jbh010204@naver.com>
2025-08-18 13:47:04 +0900

    refactor: 엔티티 LocalDateTime -> LocalDate로 변경

--- src/main/java/com/example/BE/coupon/domain/CouponEntity.java
+++ src/main/java/com/example/BE/coupon/domain/CouponEntity.java
@@ -1,9 +1,18 @@
 package com.example.BE.coupon.domain;
 
-import jakarta.persistence.*;
-import lombok.*;
-
-import java.time.LocalDateTime;
+import jakarta.persistence.Access;
+import jakarta.persistence.AccessType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "coupons")
@@ -26,10 +35,10 @@ public class CouponEntity {
     private String description;
 
     @Column(name = "valid_until")
-    private LocalDateTime validUntil;
+    private LocalDate validUntil;
 
     @Builder
-    public CouponEntity(Long storeId, String name, String description, LocalDateTime validUntil) {
+    public CouponEntity(Long storeId, String name, String description, LocalDate validUntil) {
         this.storeId = storeId;
         this.name = name;
         this.description = description;
