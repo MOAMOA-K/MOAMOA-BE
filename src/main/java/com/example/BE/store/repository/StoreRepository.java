6df812006fd608e77bfb6ca3c173eba1526dc19d
Hyun <hyun_7586@naver.com>
2025-08-23 21:52:49 +0900

    refactor: findByCanonicalNameContaining()함수를 findByCanonicalName()으로 수정

--- src/main/java/com/example/BE/store/repository/StoreRepository.java
+++ src/main/java/com/example/BE/store/repository/StoreRepository.java
@@ -10,7 +10,7 @@ import org.springframework.data.repository.query.Param;
 public interface StoreRepository extends JpaRepository<StoreEntity, Long> {
 
     // 키워드 검색 (공백 없는 이름으로 검색)
-    Optional<StoreEntity> findByCanonicalNameContaining(String canonicalName);
+    Optional<StoreEntity> findByCanonicalName(String canonicalName);
 
     // 이거는 근데 AI 도움을 받은 거라 테스트 좀 해 봐야 해요.
     // 지도 검색을 위한 공간 쿼리 (MySQL 기준)
